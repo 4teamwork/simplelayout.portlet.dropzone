7bb4a0034b71fa752c47b51ce20022bb3a9612f9
Jonas Baumann <jone@jone.ch>
2012-10-26 16:02:26 +0200

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,35 +1,43 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.1'
+version = '1.2.dev0'
 
 setup(name='simplelayout.portlet.dropzone',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
+
       keywords='',
-      author='Mathias LEIMGRUBER (4teamwork)',
-      author_email='m.leimgruber@4teamwork.ch',
-      url='http://plone.org/products/simplelayout.portlet.dropzone',
-      license='GPL',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/simplelayout.portlet.dropzone',
+      license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['simplelayout', 'simplelayout.portlet'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
-          'setuptools',
-          'plone.app.portlets',
-          'simplelayout.base'
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'plone.app.portlets',
+        'simplelayout.base'
+        # -*- Extra requirements: -*-
+        ],
+
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
